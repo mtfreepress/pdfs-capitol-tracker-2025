eaa993c0fd19769d35c51224490c508ce7566955
Jacob Olness <jolness1@gmail.com>
2025-04-26 19:12:11 -0600

    shell script

--- fetch-and-deploy.sh
+++ fetch-and-deploy.sh
@@ -2,10 +2,36 @@
 
 set -e  # Exit on error
 
+# 2025 arguments
+sessionId=2 
+sessionOrdinal=20251 
+legislatureOrdinal=69
+
+# measure time taken for a command
+measure_time() {
+    local start_time=$(date +%s)
+    echo "Running: $@"
+    "$@"
+    local end_time=$(date +%s)
+    local elapsed_time=$((end_time - start_time))
+    echo "Time taken: ${elapsed_time} seconds"
+}
+
 echo "=== Starting PDF fetch process ==="
+# grab bill list from legislative-interface
+python3 main/fetch_bill_list.py $sessionId
+# grab fiscal notes
+python3 main/fetch_fiscal_notes.py "$sessionId" "$legislatureOrdinal" "$sessionOrdinal"
+# grab legal nots
+python3 main/fetch_legal_notes.py "$sessionId" "$legislatureOrdinal" "$sessionOrdinal"
+# grab amendments
+python3 main/fetch_amendments.py "$sessionId" "$legislatureOrdinal" "$sessionOrdinal"
+# compress pdfs
+python3 main/compress_pdfs.py $sessionId
+# generate links? — figure out how we integrate them with the website
 
 # Run the fetch script
-python main/fetch_pdfs.py
+# python main/fetch_pdfs.py
 
 # echo "=== Starting PDF deploy process ==="
 
